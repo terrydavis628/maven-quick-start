3285f2b326a7802bc406981e915ee4432fe572f8
Terry Davis <davist15@email.franklin.edu>
2018-01-10 14:10:08 -0500

    Use StringUtils and added dependency to lang3

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,6 +2,7 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
@@ -17,11 +18,18 @@ public class Application {
 			System.out.println("Greeting: " + greeting);
 		}
 	}
+	
+	public int countWords(String words) {
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		int count = app.countWords("I have four words");
+		System.out.println("Words Count: " + count);
     }
 }
